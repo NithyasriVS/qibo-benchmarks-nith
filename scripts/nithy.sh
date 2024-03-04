fb64410e2cae791c16a2180007104e7ee56756e8
NithyasriVS <nithyasri.srivathsan.iyengar@gmail.com>
2024-03-04 16:37:31 +0800

    qibonew

--- scripts/nithy.sh
+++ scripts/nithy.sh
@@ -11,9 +11,6 @@
 
 for nqubits in 16 18 20 22 24 26 28
 do
-    CUDA_VISIBLE_DEVICES=0 python compare.py --circuit $circuit --nqubits $nqubits --filename $filename \
-                                             --library-options backend=qibotn,platform=cuquantum --nreps $nreps_gpu --precision $precision
-    echo
     CUDA_VISIBLE_DEVICES="" python compare.py --circuit $circuit --nqubits $nqubits --filename $filename \
                                               --library-options backend=qibotn,platform=quimb --nreps $nreps_cpu --precision $precision
     echo
